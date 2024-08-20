c3faf68b9a42f390fe91cbde0cac746856a11e28
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-08-20 01:04:27 -0700

    carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,23 @@
+import daisyui from "daisyui";
 import defaultTheme from "tailwindcss/defaultTheme";
 import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
         serif: ["Spectral", ...defaultTheme.fontFamily.serif],
-        sans: ["Lato", ...defaultTheme.fontFamily.serif],
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [
-    plugin(function ({ addBase, theme }) {
+    plugin(function ({ addBase, addUtilities, theme }) {
       addBase({
         h1: {
           fontSize: theme("fontSize.5xl"),
@@ -29,6 +33,13 @@ export default {
         },
         a: { textDecoration: "underline" },
       });
+      addUtilities({
+        ".width-content": {
+          width: `min(100%, ${theme("screens.xl")})`,
+          margin: "0 auto",
+        },
+      });
     }),
+    daisyui,
   ],
 };
