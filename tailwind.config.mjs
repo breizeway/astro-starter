63d660d19f4d13ba4338c9506edc0e846996cf89
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-08-23 13:37:50 -0700

    wc

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,7 @@ import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       screens: {
@@ -21,7 +18,7 @@ export default {
     },
   },
   plugins: [
-    plugin(function ({ addBase, addUtilities, theme }) {
+    plugin(function ({ addBase, addUtilities, addComponents, theme }) {
       addBase({
         h1: {
           fontSize: theme("fontSize.5xl"),
@@ -38,10 +35,47 @@ export default {
         a: { textDecoration: "underline" },
       });
       addUtilities({
-        ".width-content": {
+        ".site-width-content": {
           width: `min(100%, ${theme("screens.2xl")})`,
           "margin-left": "auto",
           "margin-right": "auto",
+        },
+        // ".glass-b": {
+        //   background: "rgba(255, 255, 255, 0.5)",
+        //   "box-shadow":
+        //     "inset 1px 2px 2px rgba(255, 255, 255, 0.5), inset -1px -2px 2px rgba(0, 0, 0, 0.25), inset 32.8667px -32.8667px 32.8667px rgba(194, 194, 194, 0.1), inset -32.8667px 32.8667px 32.8667px rgba(255, 255, 255, 0.1)",
+        //   /* Note: backdrop-filter has minimal browser support */
+        //   "backdrop-filter": "blur(32.8667px)",
+        //   "border-radius": "8px",
+        //   // "border-radius": "20px",
+        // },
+      });
+      addComponents({
+        ".site-px": {
+          "transition-property": "padding",
+          "transition-duration": theme("transitionDuration.DEFAULT"),
+          "transition-timing-function": theme(
+            "transitionTimingFunction.DEFAULT",
+          ),
+          "padding-left": theme("padding.4"),
+          "padding-right": theme("padding.4"),
+          "@screen sm": {
+            "padding-left": theme("padding.6"),
+            "padding-right": theme("padding.6"),
+          },
+        },
+        ".site-py": {
+          "padding-top": theme("padding.6"),
+          "padding-bottom": theme("padding.6"),
+        },
+        ".glass-b": {
+          background: "rgba(255, 255, 255, 0.5)",
+          "box-shadow":
+            "inset 1px 2px 2px rgba(255, 255, 255, 0.5), inset -1px -2px 2px rgba(0, 0, 0, 0.25), inset 32.8667px -32.8667px 32.8667px rgba(194, 194, 194, 0.1), inset -32.8667px 32.8667px 32.8667px rgba(255, 255, 255, 0.1)",
+          /* Note: backdrop-filter has minimal browser support */
+          "backdrop-filter": "blur(32.8667px)",
+          "border-radius": "8px",
+          // "border-radius": "20px",
         },
       });
     }),
@@ -52,6 +86,8 @@ export default {
       {
         light: {
           ...require("daisyui/src/theming/themes")["light"],
+
+          "base-100": "#ddd",
           "--site-logo-color-1": "#455270",
           "--site-logo-color-2": "#253040",
           "--site-logo-color-3": "#6E7B8C",
