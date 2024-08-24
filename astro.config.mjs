8dee8ced0e9ec9d04262cb6263144fdc76b99845
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-08-23 23:38:57 -0700

    fix carousel in safari

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import netlify from "@astrojs/netlify";
-import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
